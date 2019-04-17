368aeeab4a159d51c9df7047ffa3cd9db9926c7d
Leo <leo881003@gmail.com>
2019-04-17 17:39:34 +0800

    Update oh my zsh installation

--- shell/install.sh
+++ shell/install.sh
@@ -4,6 +4,10 @@ DIR="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )"
 echo "source $DIR/shell.sh" >> .bashrc
 echo "source $DIR/shell.sh" >> .zshrc
 
-if [ ! -f "${HOME}/.oh-my-zsh/oh-my-zsh.sh" ]; then
-    git clone 'https://github.com/robbyrussell/oh-my-zsh' "$HOME/.oh-my-zsh"
+if [ ! -n "$ZSH" ]; then
+    ZSH="$HOME/.oh-my-zsh"
+fi
+
+if [ ! -d "$ZSH" ]; then
+    git clone --depth=1 'https://github.com/robbyrussell/oh-my-zsh' "$HOME/.oh-my-zsh"
 fi
