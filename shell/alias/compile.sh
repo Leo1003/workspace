10887cf1ef9b43d3a78853359db1f9087d68a005
Leo <leo881003@gmail.com>
2019-04-17 16:55:34 +0800

    Update compile aliases

--- shell/alias/compile.sh
+++ shell/alias/compile.sh
@@ -1,1 +1,15 @@
-alias g++11='g++ -std=c++11 -O3'
+gccansi() {
+    gcc -lm -lcrypt -O2 -ansi -o "${1%.*}" $1
+}
+gcc11() {
+    gcc -lm -lcrypt -O2 -std='c11' -o "${1%.*}" $1
+}
+gpp11() {
+    g++ -lm -lcrypt -O2 -std='c++11' -o "${1%.*}" $1
+}
+gpp14() {
+    g++ -lm -lcrypt -O2 -std='c++14' -o "${1%.*}" $1
+}
+gpp17() {
+    g++ -lm -lcrypt -O2 -std='c++17' -o "${1%.*}" $1
+}
