c5190a7386a728709d40705aa9f7855a78c383f7
Leo <leo881003@gmail.com>
2019-05-18 19:57:54 +0800

    Improve Compile Alias

--- shell/alias/compile.sh
+++ shell/alias/compile.sh
@@ -1,15 +1,35 @@
+get_output_name() {
+    for arg in "$@"
+    do
+        if [ -f $arg ]; then 
+            echo "${arg%.*}"
+            return
+        fi
+    done
+    echo "a.out"
+}
+
 gccansi() {
-    gcc -lm -lcrypt -O2 -ansi -o "${1%.*}" $1
+    name=$(get_output_name "$@")
+    gcc -lm -lcrypt -O2 -ansi -o "$name" "$@"
+}
+gcc99() {
+    name=$(get_output_name "$@")
+    gcc -lm -lcrypt -O2 -std='c99' -o "$name" "$@"
 }
 gcc11() {
-    gcc -lm -lcrypt -O2 -std='c11' -o "${1%.*}" $1
+    name=$(get_output_name "$@")
+    gcc -lm -lcrypt -O2 -std='c11' -o "$name" "$@"
 }
 gpp11() {
-    g++ -lm -lcrypt -O2 -std='c++11' -o "${1%.*}" $1
+    name=$(get_output_name "$@")
+    g++ -lm -lcrypt -O2 -std='c++11' -o "$name" "$@"
 }
 gpp14() {
-    g++ -lm -lcrypt -O2 -std='c++14' -o "${1%.*}" $1
+    name=$(get_output_name "$@")
+    g++ -lm -lcrypt -O2 -std='c++14' -o "$name" "$@"
 }
 gpp17() {
-    g++ -lm -lcrypt -O2 -std='c++17' -o "${1%.*}" $1
+    name=$(get_output_name "$@")
+    g++ -lm -lcrypt -O2 -std='c++17' -o "$name" "$@"
 }
