3545424f6400dee1118696fc9d95ef21cee9f127
Leo <leo881003@gmail.com>
2019-05-18 19:33:57 +0800

    Add PATH exporting

--- shell/configures/environments.sh
+++ shell/configures/environments.sh
@@ -2,3 +2,10 @@ export GOPATH=~/go
 export PATH=~/bin:$PATH
 export VISUAL=vim
 export EDITOR=vim
+if [ -d "$HOME/go/bin" ]; then
+    export PATH="$HOME/go/bin":$PATH
+fi
+
+if [ -d "$HOME/.cargo/bin" ]; then
+    export PATH="$HOME/.cargo/bin":$PATH
+fi
