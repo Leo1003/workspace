6e93254488e85ddaefe212a891ac822b18daa2ee
Leo <leo881003@gmail.com>
2019-10-06 07:18:02 +0800

    Update tmux settings

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 DIR="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )"
 
-cp -R -v $DIR/themes $HOME/.tmux
+# cp -R -v $DIR/themes $HOME/.tmux
 cp -v $DIR/config.conf $HOME/.tmux.conf
+
+if [ ! -f "${HOME}/.tmux/plugins/tpm/tpm" ]; then 
+    echo 'Installing Tmux Plugin Manager...'
+    git clone 'https://github.com/tmux-plugins/tpm' "${HOME}/.tmux/plugins/tpm"
+fi
+
