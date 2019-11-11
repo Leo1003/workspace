a83eefdbff27f31a7f818680011756641ab11355
Leo <leo881003@gmail.com>
2019-11-12 02:52:22 +0800

    Use plugin of tender theme instead of file

--- vim/install.sh
+++ vim/install.sh
@@ -2,10 +2,9 @@
 DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 cp -v $DIR/vimrc $HOME/.vimrc
-mkdir -p $HOME/.vim
-cp -R -v $DIR/colors $HOME/.vim/
 
 if [ ! -f $HOME/.vim/autoload/plug.vim ]; then
     echo 'Installing vim-plug...'
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 fi
+
