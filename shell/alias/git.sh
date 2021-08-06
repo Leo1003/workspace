149c0f4cd8b86a9bc81eb08e3e7afaa3f5fb56fd
Leo <leo881003@gmail.com>
2021-08-07 00:51:26 +0800

    Add git debug functions

--- shell/alias/git.sh
+++ shell/alias/git.sh
@@ -1,1 +1,55 @@
 alias gstre='git stash show -p | git apply --reverse'
+
+git-ls-all-objects() {
+    local obj_path="$(git rev-parse --git-path objects)"
+
+    # packed objects
+    find "$obj_path/pack" -regextype posix-extended -regex '.*pack-([0-9a-f]*).idx' -exec cat '{}' \; | git show-index | cut -f 2 -d ' '
+
+    # loose objects
+    find "$obj_path" -regextype posix-extended -regex '.*[0-9a-f]{2}/([0-9a-f]*)' | rev | cut -d '/' -f 1-2 | rev | tr -d '/'
+}
+
+_filter_object_type() {
+    git-ls-all-objects | while IFS= read -r obj; do
+        obj_type="$(git cat-file -t $obj)"
+        if [ "$obj_type" = "$1" ]; then
+            echo "$obj"
+        fi
+    done
+}
+
+git-ls-all-commits() {
+    _filter_object_type "commit"
+}
+
+git-ls-all-trees() {
+    _filter_object_type "tree"
+}
+
+git-ls-all-blobs() {
+    _filter_object_type "blob"
+}
+
+git-ls-all-tags() {
+    _filter_object_type "tag"
+}
+
+git-find-tree-in-commits() {
+    local searchfor="${1:?No tree hash provided.}"
+    local startpoints="${2:-HEAD}"
+
+    if [ "$startpoints" = "--" ]; then
+        cat
+    else
+        git rev-list $startpoints
+    fi | while read commithash; do
+        if git cat-file -p $commithash | grep "tree $searchfor"; then
+            echo " -- found at $commithash"
+        fi
+        if git ls-tree -d -r --full-tree $commithash | grep $searchfor; then
+            echo " -- found at $commithash"
+        fi
+    done
+}
+
