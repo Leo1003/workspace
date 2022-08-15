91890c8944c583ee48d7e4298358c3331e6e6746
Leo <leo881003@gmail.com>
2022-08-16 01:28:48 +0800

    Add authorized_keys revocation

--- sshkeys/install.sh
+++ sshkeys/install.sh
@@ -2,9 +2,28 @@
 DIR="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )"
 
 add_authorized_key() {
-    if ! grep "$1" "$HOME/.ssh/authorized_keys" > /dev/null; then
+    if [ -z "$1" ]; then
+        return
+    fi
+    local part_key=`echo "$1" | cut -d ' ' -f 1-2`
+
+    if ! grep "$part_key" "$HOME/.ssh/authorized_keys" > /dev/null; then
         echo "$1" >> "$HOME/.ssh/authorized_keys"
         echo "Installed ssh key: $(echo "$1" | ssh-keygen -lf /dev/stdin)"
+    else
+        echo "Existed ssh key: $(echo "$1" | ssh-keygen -lf /dev/stdin)"
+    fi
+}
+
+rm_authorized_key() {
+    if [ -z "$1" ]; then
+        return
+    fi
+    local part_key=`echo "$1" | cut -d ' ' -f 1-2`
+
+    if grep "$part_key" "$HOME/.ssh/authorized_keys" > /dev/null; then
+        sed -i "\\|$part_key|d" "$HOME/.ssh/authorized_keys"
+        echo "Revoked ssh key: $(echo "$1" | ssh-keygen -lf /dev/stdin)"
     fi
 }
 
@@ -18,3 +37,10 @@ while IFS= read -r pubkey; do
         add_authorized_key "$pubkey"
     fi
 done < "$DIR/authorized_keys"
+
+while IFS= read -r pubkey; do
+    if [ -n "$pubkey" ]; then
+        rm_authorized_key "$pubkey"
+    fi
+done < "$DIR/revoked_keys"
+
