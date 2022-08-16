a764e4f2187138e92fd67fb99e1073b5c16a3219
Leo <leo881003@gmail.com>
2022-08-16 15:32:05 +0800

    Add support for wayland clipboard

--- shell/alias/clipboard.sh
+++ shell/alias/clipboard.sh
@@ -1,7 +1,14 @@
-if has_command xclip; then
-    alias xpaste='xclip -selection clipboard -o'
-    alias xcopy='xclip -selection clipboard -i'
-    fcopy() {
-        cat "$1" | xcopy
-    }
+if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
+    if has_command wl-copy; then
+        alias copy='wl-copy -n'
+    fi
+    if has_command wl-paste; then
+        alias paste='wl-paste -n'
+    fi
+elif [ "$XDG_SESSION_TYPE" = "x11" ]; then
+    if has_command xclip; then
+        alias paste='xclip -selection clipboard -o'
+        alias copy='xclip -selection clipboard -i'
+    fi
 fi
+
