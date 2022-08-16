aa91f2e9801dea0e804b78c4e2bf310f02248c90
Leo <leo881003@gmail.com>
2022-08-16 15:39:05 +0800

    Improve ssh-agent installation

--- ssh-agent/install.sh
+++ ssh-agent/install.sh
@@ -6,6 +6,8 @@ set -e
 mkdir -p "$HOME/.config/systemd/user"
 cp -i "$DIR/ssh-agent.service" "$HOME/.config/systemd/user"
 
-systemctl --user --daemon-reload
 systemctl --user enable --now ssh-agent.service
+
+mkdir -p "$HOME/.config/environment.d"
+echo "SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/ssh-agent.socket" > "$HOME/.config/environment.d/10-ssh-agent.conf"
 
