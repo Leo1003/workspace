e42c018462a3e1feb263dceeb402188bef33ee47
Leo <leo881003@gmail.com>
2022-08-24 12:05:54 +0800

    Fix GPG password prompt

--- shell/configures/environments.sh
+++ shell/configures/environments.sh
@@ -2,6 +2,7 @@ export GOPATH=~/go
 export PATH=~/bin:~/.local/bin:$PATH
 export VISUAL=vim
 export EDITOR=vim
+export GPG_TTY=$(tty)
 if [ -d "$HOME/go/bin" ]; then
     export PATH="$HOME/go/bin":$PATH
 fi
