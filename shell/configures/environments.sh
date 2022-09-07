b877aae23a03e71d601127bdca8105a1a6f07059
Leo <leo881003@gmail.com>
2022-09-07 21:40:02 +0800

    Auto detect local ssh-agent

--- shell/configures/environments.sh
+++ shell/configures/environments.sh
@@ -1,8 +1,18 @@
-export GOPATH=~/go
 export PATH=~/bin:~/.local/bin:$PATH
 export VISUAL=vim
 export EDITOR=vim
 export GPG_TTY=$(tty)
+
+if [ ! -S "$SSH_AUTH_SOCK" ]; then
+    if [ -S "$XDG_RUNTIME_DIR/ssh-agent.socket" ]; then
+        export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+    fi
+fi
+
+if [ -d "$HOME/go" ]; then
+    export GOPATH=~/go
+fi
+
 if [ -d "$HOME/go/bin" ]; then
     export PATH="$HOME/go/bin":$PATH
 fi
@@ -10,3 +20,4 @@ fi
 if [ -d "$HOME/.cargo/bin" ]; then
     export PATH="$HOME/.cargo/bin":$PATH
 fi
+
