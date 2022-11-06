02bd148ffe9ca5599a65c4bf4e8d0ce5e8f14040
Leo <leo881003@gmail.com>
2022-11-06 21:13:09 +0800

    Remove old pacman alias

--- shell/alias/pacman.sh
+++
@@ -1,9 +0,0 @@
-if has_command pacman; then
-    alias sps='sudo pacman -S'
-    alias spss='pacman -Ss'
-    alias spsi='pacman -Si'
-    alias spsy='sudo pacman -Sy'
-    alias spsyu='sudo pacman -Syu'
-    alias pacql='pacman -Ql'
-    alias pacqi='pacman -Qi'
-fi
