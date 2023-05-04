0643436a4ce738c6b8be1b4f77109e7a5bb8ada9
Leo Chen <leo881003@gmail.com>
2023-05-05 06:16:42 +0800

    Enable rust-analyzer LSP

--- nvim/NvChad_custom/configs/lspconfig.lua
+++ nvim/NvChad_custom/configs/lspconfig.lua
@@ -4,14 +4,25 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd" }
+local servers = {
+  "html",
+  "cssls",
+  "tsserver",
+  "clangd",
+  "rust_analyzer",
+}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
     capabilities = capabilities,
+    update_in_insert = true,
+    settings = {
+      ["rust-analyzer"] = {
+        check = {
+          command = "clippy",
+        },
+      },
+    },
   }
 end
-
--- 
--- lspconfig.pyright.setup { blabla}
