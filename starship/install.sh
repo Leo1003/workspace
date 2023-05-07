209f460431763b4bbc324d3941976627a2bceedf
Leo Chen <leo881003@gmail.com>
2023-05-08 06:28:08 +0800

    Remove starship Nerd symbol configs

--- starship/install.sh
+++ starship/install.sh
@@ -10,32 +10,3 @@ starship config add_newline false
 starship config cmd_duration.min_time 1
 starship config time.disabled false
 starship config username.show_always true
-
-# Nerd Symbol customization
-starship config hostname.ssh_symbol '爵 '
-starship config aws.symbol '  '
-starship config buf.symbol ' '
-starship config c.symbol ' '
-starship config conda.symbol ' '
-starship config dart.symbol ' '
-starship config directory.read_only '  '
-starship config docker_context.symbol ' '
-starship config elixir.symbol ' '
-starship config elm.symbol ' '
-#starship config git_branch.symbol ' '
-starship config golang.symbol ' '
-starship config haskell.symbol ' '
-#starship config hg_branch.symbol ' '
-starship config java.symbol ' '
-starship config julia.symbol ' '
-starship config memory_usage.symbol ' '
-starship config nim.symbol ' '
-starship config nix_shell.symbol ' '
-starship config nodejs.symbol ' '
-starship config package.symbol ' '
-starship config php.symbol ' '
-starship config python.symbol ' '
-starship config sudo.symbol ' '
-starship config spack.symbol '🅢 '
-starship config rust.symbol ' '
-
