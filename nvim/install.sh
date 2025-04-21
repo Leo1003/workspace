ca920a195a1890256e7cbebf3f1ad35ca042f63b
Leo Chen <leo881003@gmail.com>
2025-04-21 17:59:59 +0800

    nvim: Prepare for NvChad 2.5 migration

--- nvim/install.sh
+++ nvim/install.sh
@@ -2,15 +2,7 @@
 set -e
 DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
-if [ ! -d "$HOME/.config/nvim" ]; then
-    echo "Installing NvChad..." >&2
-    git clone https://github.com/NvChad/NvChad "$HOME/.config/nvim" --depth 1
-else
-    echo "Found existing configuration in ~/.config/nvim ..." >&2
-    echo "Not to install NvChad" >&2
-fi
-
-ln -sfTv "$DIR/NvChad_custom" "$HOME/.config/nvim/lua/custom"
+ln -sfTv "$DIR/NvChad_config" "$HOME/.config/nvim"
 
 # Run vim to install plugins
 
