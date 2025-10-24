e40bc8f3552d726d548eb079befc773f72a0cd5e
Leo Chen <leo881003@gmail.com>
2025-10-24 19:21:32 +0800

    shell: Improve bash experience

--- shell/alias/filesystem.sh
+++ shell/alias/filesystem.sh
@@ -1,8 +1,13 @@
-if type exa 2> /dev/null 1>&2; then
-    alias ls='exa --color=auto --git'
-elif type lsd 2> /dev/null 1>&2; then
-    alias ls='lsd --color=auto --size=short'
+if has_command eza; then
+    alias ls='eza --color=auto --classify=auto --git'
+elif has_command exa; then
+    alias ls='exa --color=auto --classify=auto --git'
+elif has_command lsd; then
+    alias ls='lsd --color=auto --classify=auto --size=short'
+else
+    alias ls='ls --color=auto --classify=auto'
 fi
+alias l='ls'
 alias lp='ls -alh'
 alias la='ls -a'
 alias ll='ls -lh'
@@ -15,5 +20,15 @@ alias cpr='cp -R'
 alias lssd='find /dev/ -type b -exec sh -c "ls --color=always -lh {}" \;'
 
 takeown() {
-    sudo chown -R "$(id -u):$(id -g)" "$@"
+    sudo chown -R "`id -u`:`id -g`" "$@"
+}
+
+# cp with rsync
+rcp() {
+    if has_command rsync; then
+        rsync -rlptcP "$@"
+    else
+        echo "Command rsync not found." >&2
+        return 255
+    fi
 }
--- shell/configures/environments.bash.sh
+++ shell/configures/environments.bash.sh
@@ -4,3 +4,13 @@ fi
 
 try_scripts "/usr/share/doc/pkgfile/command-not-found.bash"
 
+# History improvment
+shopt -s histappend
+shopt -s cmdhist
+HISTCONTROL=ignoreboth:erasedups
+HISTSIZE=50000
+if [ -z "$PROMPT_COMMAND" ]; then
+    PROMPT_COMMAND="history -a"
+else
+    PROMPT_COMMAND="$PROMPT_COMMAND; history -a"
+fi
